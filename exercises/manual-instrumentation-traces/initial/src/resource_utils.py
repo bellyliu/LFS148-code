47354f52cd3b7326fd960b1d98f59e1bed5baaca
belly-rewardz <dekri@rewardz.sg>
2026-01-20 14:58:46 +0700

    Add resource attribute

---
+++ exercises/manual-instrumentation-traces/initial/src/resource_utils.py
@@ -0,0 +1,14 @@
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.semconv.resource import ResourceAttributes
+
+def create_resource(name: str, version: str) -> Resource:
+    svc_resource = Resource.create(
+        {
+            ResourceAttributes.SERVICE_NAME: name,
+            ResourceAttributes.SERVICE_VERSION: version,
+            "service.maintainer": "dbl",
+            "k8s.pod_name": "pod-2131xasdf5",
+            "k8s.cluster": "prod-sg-eks"
+        }
+    )
+    return svc_resource
